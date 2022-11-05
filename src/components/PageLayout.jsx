63a95e830d4a32f3ff0d16a4555575c3801d72df
MariaClaudiaWandersee <mariaaclaudiaa2401@gmail.com>
2022-11-04 21:52:45 -0300

    last-update

--- src/components/PageLayout.jsx
+++ src/components/PageLayout.jsx
@@ -1,89 +1,85 @@
 import {
-    AppBar,
-    Box,
-    Drawer,
-    IconButton,
-    List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
-    Toolbar,
-    Typography,
-  } from "@mui/material";
-  import { Link, Outlet } from "react-router-dom";
-  import MenuIcon from "@mui/icons-material/Menu";
-  import { useState } from "react";
-  
-  export const PageLayout = () => {
-    const DrawerApp = () => (
-      <Box style={{ minWidth: "260px" }}>
-        <Typography variant="h5">Menu</Typography>
-        <List>
-          <ListItem>
-            <ListItemButton component="a" href="/">
-              <ListItemText primary={"Home"} />
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton component="a" href="/album">
-              <ListItemText primary={"Álbum"} />
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton component="a" href="/sobre">
-              <ListItemText primary={"Sobre"} />
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton>
-              <ListItemText primary={"Notícias"} component="a" href="/noticias" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem>
-            <ListItemButton>
-              <ListItemText primary={"Contatos"} component="a" href="/contatos" />
-            </ListItemButton>
-          </ListItem>
-        </List>
+  AppBar,
+  Box,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Link, Outlet } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState } from "react";
+
+export const PageLayout = () => {
+  const DrawerApp = () => (
+    <Box style={{ minWidth: "260px" }}>
+      <Typography variant="h5">Menu</Typography>
+      <List>
+        <ListItem>
+          <ListItemButton to="/" LinkComponent={Link}>
+            <ListItemText primary={"Home"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton to="/album" LinkComponent={Link}>
+            <ListItemText primary={"Álbum"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton component="a" href="/sobre">
+            <ListItemText primary={"Sobre"} />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton to="/noticias" LinkComponent={Link}>
+            <ListItemText primary={"Notícias"} component="a" href="/noticias" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ListItemButton to="/contato" LinkComponent={Link}>
+            <ListItemText primary={"Contatos"} />
+          </ListItemButton>
+        </ListItem>
+      </List>
+    </Box>
+  );
+
+  const ContainerMenu = () => {
+    const [aberto, setAberto] = useState(false);
+
+    const lideComOMenu = () => {
+      setAberto(!aberto);
+    };
+
+    return (
+      <Box>
+        <AppBar>
+          <Toolbar>
+            <IconButton onClick={lideComOMenu}>
+              <MenuIcon></MenuIcon>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Box>
+          <Drawer open={aberto} onClose={lideComOMenu}>
+            <DrawerApp />
+          </Drawer>
+        </Box>
       </Box>
     );
-  
-    const ContainerMenu = () => {
-      const [aberto, setAberto] = useState(false);
-  
-      const lideComOMenu = () => {
-        setAberto(!aberto); 
-      };
-  
-      return (
-        <Box>
-          <AppBar>
-            <Toolbar>
-              <IconButton onClick={lideComOMenu}>
-                <MenuIcon></MenuIcon>
-              </IconButton>
-            </Toolbar>
-          </AppBar>
-          <Box>
-            <Drawer 
-              open={aberto} 
-              onClose={lideComOMenu}
-            >
-              <DrawerApp />
-            </Drawer>
-          </Box>
-        </Box>
-      );
-    };
-  
-    return (
-      <>
-        <ContainerMenu />
-        <Box style={{ marginTop: "80px" }}>
-          <Outlet />
-        </Box>
-      </>
-    );
   };
-  
+
+  return (
+    <>
+      <ContainerMenu />
+      <Box style={{ marginTop: "80px" }}>
+        <Outlet />
+      </Box>
+    </>
+  );
+};
--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -12,6 +12,7 @@ import {
 import { PageLayout } from "../components/PageLayout";
 import "../App.css";
 import { Grid } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const ErrorPage = () => (
   <div>
@@ -23,12 +24,8 @@ const ErrorPage = () => (
 const HomeScreen = () => (
   <div class="home">
     {/* <h1>Bem Vindo à Samsung, sua loja favorita..</h1> */}
-    <a href="https://shop.samsung.com/br/mobile/smartphone?page=2">
-      <img
-        class="home-img"
-        alt="svgImg"
-        src="https://t.ctcdn.com.br/V99BnwJnzsGPEzzukD2tsbfh4rc=/400x400/smart/i489963.jpeg"
-      />
+    <a href="https://www.amazon.com.br/?&tag=hydrbrabk-20&ref=pd_sl_7rwd1q78df_e&adgrpid=79547423725&hvpone=&hvptwo=&hvadid=591863875878&hvpos=&hvnetw=g&hvrand=10441088185878839049&hvqmt=e&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9102332&hvtargid=kwd-10573980&hydadcr=26346_11691057&gclid=EAIaIQobChMIocaC3-OV-wIVDtORCh2rtQyOEAAYASAAEgIuzfD_BwE">
+      <img class="home-img" alt="svgImg" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1200px-Amazon_logo.svg.png"/>
     </a>
   </div>
 );
@@ -36,35 +33,34 @@ const HomeScreen = () => (
 //Album
 
 const Album = () => {
+  // fetch products from a fake API
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setProducts(json));
+  });
+
   return (
     <div>
       <Grid container spacing={3}>
         <Grid item>
-          <h1 class="">
+          <h1 class="title-supreme">
             Veja algumas imagens de nossos produtos mais vendidos no ano de 2022
           </h1>
         </Grid>
-        <Grid item md={6} xs={12}>
-          <h3>Galaxy S21 FE 5G Verde 128GB</h3>
-          <img class="img-cell" alt="cel1" href="" src="https://samsungbr.vtexassets.com/arquivos/ids/330260-800-auto?"/>
-          <a href="https://shop.samsung.com/br/checkout/#/cart">
-            <button>Comprar</button>
-          </a>
-        </Grid>
-        <Grid item md={6} xs={12}>
-          <h3>Galaxy S22 Ultra 5G Preto 256GB</h3>
-          <img class="img-cell" alt="cel2" src="https://samsungbr.vtexassets.com/arquivos/ids/331116-800-auto"/>
-          <a href="https://shop.samsung.com/br/galaxy-s22-ultra-5g/instant-voucher?skuId=2509">
-            <button>Comprar</button>
-          </a>
-        </Grid>
-        <Grid item md={6} xs={12}>
-          <h3>Galaxy Tab S7 FE LTE Prata 128GB</h3>
-          <img class="img-cell" alt="cel3" src="https://samsungbr.vtexassets.com/arquivos/ids/336859-800-auto"/>
-          <a href="https://shop.samsung.com/br/checkout/#/cart">
-            <button>Comprar</button>
-          </a>
-        </Grid>
+        {products.map((product) => (
+          <Grid item xs={12} sm={6} md={4} lg={3}>
+            <h3 class="title"> {product.title}</h3>
+            {product.price}
+            <img class="img-cell" alt="cel1" href="" src={product.image} />
+            <a
+              href={`https://www.google.com/search?q=${product.title}&tbm=shop`}
+            >
+              <button class="comprar">Comprar</button>
+            </a>
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
@@ -74,71 +70,73 @@ const Album = () => {
 const Sobre = () => (
   <div class="sobre">
     <h1>Sobre a Samsung, a maior loja de Eletronicos do mundo.</h1>
-      <p class="text-about">
-        A Samsung nasceu na Coreia do Sul em 1938 e se transformou em um dos
-        maiores conglomerados do país e do mundo, com atuação em diversas áreas.
-        Apesar da diversidade dos negócios, ela é mais conhecida globalmente
-        pelos eletrônicos. Fundada por Lee Byung-chul, a empresa teve origens
-        modestas, dedicada à exportação de alimentos.
-        Em poucos anos, a Samsung
-        ampliou os negócios para entrar também em outros setores, incluindo
-        comércio, finanças e têxtil. A divisão de produtos eletrônicos só surgiu
-        em 1969, com o lançamento de sua primeira TV, ainda em preto e branco. A
-        empresa começou a expansão internacional e ficou conhecida também por
-        eletrodomésticos, como geladeiras e máquinas de lavar.
-        O crescimento da empresa levou os negócios para a fabricação de monitores de
-        computadores, aparelhos celulares, tablets, robôs, entre tantos outros
-        produtos tecnológicos. A partir da década de 2010, a companhia se
-        estabelece como uma das principais marcas do mercado de tecnologia
-        móvel, tornando-se a principal rival da Apple.
-      </p>
-      <p class="text-about">
-        A disputa gerou um dos processos mais famosos da história da tecnologia, iniciado em 2011 e
-        encerrado apenas em 2018, em que a Samsung foi acusada de infringir
-        patentes referentes ao iPhone e ao iOS em seus celulares. A empresa é
-        conhecida pela linha de smartphones Galaxy S, mas também foi uma das
-        líderes no mercado de celulares com telas dobráveis, com produtos como o
-        Galaxy Z Fold e o Z Flip.
-        A companhia ainda produz os relógios da família Galaxy Watch e os fones de ouvido Galaxy Buds. A Samsung chegou
-        ao Brasil em 1986 com duas linhas de produtos: monitores e discos
-        rígidos. Com a rápida ascensão das operações brasileiras, a companhia
-        passou a investir em unidades de fabricação locais para aumentar o
-        portfólio disponível no país. A companhia conta com duas unidades fabris
-        locais: uma localizada em Manaus (AM), e a outra em Campinas (SP).
-      </p>
-      <img alt="sede_samsung" src="https://img.global.news.samsung.com/br/wp-content/uploads/2022/01/CES-Booth-Photo-Sketch_main1.jpg"/>
+    <p class="text-about">
+      A Samsung nasceu na Coreia do Sul em 1938 e se transformou em um dos
+      maiores conglomerados do país e do mundo, com atuação em diversas áreas.
+      Apesar da diversidade dos negócios, ela é mais conhecida globalmente pelos
+      eletrônicos. Fundada por Lee Byung-chul, a empresa teve origens modestas,
+      dedicada à exportação de alimentos. Em poucos anos, a Samsung ampliou os
+      negócios para entrar também em outros setores, incluindo comércio,
+      finanças e têxtil. A divisão de produtos eletrônicos só surgiu em 1969,
+      com o lançamento de sua primeira TV, ainda em preto e branco. A empresa
+      começou a expansão internacional e ficou conhecida também por
+      eletrodomésticos, como geladeiras e máquinas de lavar. O crescimento da
+      empresa levou os negócios para a fabricação de monitores de computadores,
+      aparelhos celulares, tablets, robôs, entre tantos outros produtos
+      tecnológicos. A partir da década de 2010, a companhia se estabelece como
+      uma das principais marcas do mercado de tecnologia móvel, tornando-se a
+      principal rival da Apple.
+    </p>
+    <p class="text-about">
+      A disputa gerou um dos processos mais famosos da história da tecnologia,
+      iniciado em 2011 e encerrado apenas em 2018, em que a Samsung foi acusada
+      de infringir patentes referentes ao iPhone e ao iOS em seus celulares. A
+      empresa é conhecida pela linha de smartphones Galaxy S, mas também foi uma
+      das líderes no mercado de celulares com telas dobráveis, com produtos como
+      o Galaxy Z Fold e o Z Flip. A companhia ainda produz os relógios da
+      família Galaxy Watch e os fones de ouvido Galaxy Buds. A Samsung chegou ao
+      Brasil em 1986 com duas linhas de produtos: monitores e discos rígidos.
+      Com a rápida ascensão das operações brasileiras, a companhia passou a
+      investir em unidades de fabricação locais para aumentar o portfólio
+      disponível no país. A companhia conta com duas unidades fabris locais: uma
+      localizada em Manaus (AM), e a outra em Campinas (SP).
+    </p>
+    <img alt="sede_samsung" src="https://img.global.news.samsung.com/br/wp-content/uploads/2022/01/CES-Booth-Photo-Sketch_main1.jpg"/>
   </div>
 );
 
 //Noticias
 const Noticias = () => (
-  <div>
-    <h1>Notícias</h1>
+  <div class="noticias">
+    <h1>Amazon fecha acordo com Agência Espacial Brasileira</h1>
     <div>
-      <p>teste</p>
+      <p>A Amazon Web Services (AWS) — plataforma de serviços de computação em nuvem do grupo Amazon</p>
+      <img alt="" src="https://www.rbsdirect.com.br/filestore/4/5/9/1/6/9/1_c821f4debbba6b3/1961954_9d09d724e4ea775.jpg"/>
     </div>
   </div>
 );
 
 //Contatos
 const Contatos = () => (
-  <div>
+  <div class="contato">
     <h1>Fale Conosco</h1>
     <div>
       <h4>Telefone</h4>
       <p>
-        {" "}
         4004-0000 (Capitais e grandes centros) 0800 555 0000 (Demais cidades e
+        <br />
         regiões) De Seg. a Sex. das 8h às 22h, Sáb. e Dom. das 9h às 18h Conheça
         o nosso atendimento telefônico digital.
+        <br />
       </p>
     </div>
     <div>
       <h4>Loja Online</h4>
       <p>
-        {" "}
         Para informações referentes à sua compra, ligue para 0800 007 2611. De
+        <br />
         Seg. a Sex. das 8h às 20h e Sábado das 9h às 18h, exceto feriados.
+        <br />
       </p>
     </div>
   </div>
@@ -148,10 +146,10 @@ const route = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<PageLayout />} errorElement={<ErrorPage />}>
       <Route index element={<HomeScreen />} />
-      <Route index path="album" element={<Album />} />
-      <Route path="sobre" element={<Sobre />} />
-      <Route path="noticias" element={<Noticias />} />
-      <Route path="contatos" element={<Contatos />} />
+      <Route path="/album" element={<Album />} />
+      <Route path="/sobre" element={<Sobre />} />
+      <Route path="/noticias" element={<Noticias />} />
+      <Route path="/contato" element={<Contatos />} />
     </Route>
   )
 );
